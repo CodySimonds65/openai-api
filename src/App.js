d5d0bc1d568c7ee4ff710508a55c9b0e97ae933e
dweebstify <dweebstify@gmail.com>
2024-04-14 03:08:35 -0500

    Main Boilerplate completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
+import "./index.css";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <section className="side-bar">
         <button className="side-bar__button">+ New chat</button>
         <ul className="history">
@@ -11,10 +11,19 @@ const App = () => {
           <li className="side-bar__list-item">Chat 3</li>
         </ul>
         <nav className="side-bar__nav">
-          <p>Cody's firsy API test</p>
+          <p>Openai API Test Project</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <header className="main__header">
+          <h1>Cody GPT Test</h1>
+        </header>
+        <ul className="feed"></ul>
+
+        <div className="bottom-section">
+          <div className="input-container"></div>
+        </div>
+      </section>
     </div>
   );
 };
